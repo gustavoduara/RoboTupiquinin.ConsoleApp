a6218fd5ab8d73bf0a19d0309bff3001b1fb5444
Gustavo Duara <gugaduara@gmail.com>
2025-04-01 15:08:26 -0300

    feito refatoração orientada em objetos

--- RoboTupiquinin.ConsoleApp/Program.cs
+++ RoboTupiquinin.ConsoleApp/Program.cs
@@ -1,112 +1,40 @@
-﻿namespace RoboTupiniquimin.ConsoleApp
+﻿namespace RoboTupiniquim
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("---------------------------------------------");
-            Console.WriteLine("Robo Tupiniquim - Missão Marte");
-            Console.WriteLine("---------------------------------------------");
+            string[] posicoesIniciais = ["1 2 N", "3 3 L"];
+            string[] comandosIniciais = ["EMEMEMEMM", "MMDMMDMDDM"];
 
-            do
+            string[] nomesRobos = ["Gabriel", "Leonardo"];
+
+            int quantidadeRobos = 2;
+
+            Robo[] robos = new Robo[quantidadeRobos];
+
+            for (int contador = 0; contador < robos.Length; contador++)
             {
-                ExecutarRobo("PRIMEIRO");
+                string[] coordenadasAtuais = posicoesIniciais[contador].Split(' ');
 
-                Console.WriteLine("Pressione ENTER para iniciar o SEGUNDO robô...");
-                Console.ReadLine();
+                Robo robo = new Robo();
 
-                ExecutarRobo("SEGUNDO");
+                robo.nome = nomesRobos[contador];
+                robo.posicaoX = Convert.ToInt32(coordenadasAtuais[0]);
+                robo.posicaoY = Convert.ToInt32(coordenadasAtuais[1]);
+                robo.direcao = Convert.ToChar(coordenadasAtuais[2]);
 
-                Console.WriteLine("Deseja continuar? (S para Sim, N para Não): ");
-                string resposta = Console.ReadLine().ToUpper();
+                char[] instrucoes = comandosIniciais[contador].ToCharArray();
 
-                if (resposta != "S")
-                {
-                    break;
-                }
-                else
-                {
-                    Console.Clear(); 
-                    Console.WriteLine("Reiniciando...");
-                }
+                robo.Explorar(instrucoes);
 
-            } while (true); 
-        }
+                robos[contador] = robo;
+            }
 
-        static void ExecutarRobo(string nomeRobo)
-        {
-            Console.Write($"Informe os valores do tamanho do {nomeRobo} Grid (X espaço Y): ");
-            string[] grid = Console.ReadLine().Split(' ');
-            int limiteX = int.Parse(grid[0]);
-            int limiteY = int.Parse(grid[1]);
+            foreach (Robo robo in robos)
+                Console.WriteLine(robo.ObterLocalizacao());
 
-            Console.WriteLine($"Informe a posição inicial do {nomeRobo} Robô no grid (X e Y) e o sentido que ele está: (ex: N,S,L, ou O):  ");
-            string[] posicoes = Console.ReadLine().Split(' ');
-            int posicaoX = int.Parse(posicoes[0]);
-            int posicaoY = int.Parse(posicoes[1]);
-            char direcao = char.Parse(posicoes[2]);
-
-            Console.WriteLine($"Informe os comandos que o {nomeRobo} Robô deve executar:(Ex:EMEMEMEMM) ");
-            char[] instrucoes = Console.ReadLine().ToCharArray();
-
-            (posicaoX, posicaoY, direcao) = MoverRobo(posicaoX, posicaoY, direcao, instrucoes, limiteX, limiteY);
-
-            Console.WriteLine($"{posicaoX} {posicaoY} {direcao}");
             Console.ReadLine();
         }
-
-        static (int, int, char) MoverRobo(int x, int y, char direcao, char[] instrucoes, int limiteX, int limiteY)
-        {
-            foreach (char instrucao in instrucoes)
-            {
-                switch (instrucao)
-                {
-                    case 'D':
-                        direcao = VirarDireita(direcao);
-                        break;
-                    case 'E':
-                        direcao = VirarEsquerda(direcao);
-                        break;
-                    case 'M':
-                        (x, y) = Movimentar(x, y, direcao);
-
-                        if (x < 0 || y < 0 || x > limiteX || y > limiteY)
-                        {
-                            Console.WriteLine("O robô saiu do grid!");
-                            return (x, y, direcao);
-                        }
-                        break;
-                }
-            }
-            return (x, y, direcao);
-        }
-
-        static char VirarDireita(char direcao) => direcao switch
-        {
-            'N' => 'L',
-            'L' => 'S',
-            'S' => 'O',
-            'O' => 'N',
-            _ => direcao
-        };
-
-        static char VirarEsquerda(char direcao) => direcao switch
-        {
-            'N' => 'O',
-            'O' => 'S',
-            'S' => 'L',
-            'L' => 'N',
-            _ => direcao
-        };
-
-        static (int, int) Movimentar(int x, int y, char direcao) => direcao switch
-        {
-            'N' => (x, y + 1),
-            'S' => (x, y - 1),
-            'L' => (x + 1, y),
-            'O' => (x - 1, y),
-            _ => (x, y)
-        };
     }
 }
